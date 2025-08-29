9ca69298ef3e15008ab4a018684e049725176567
Satish Singh <i.sksingh113@gmail.com>
2025-08-29 14:26:57 +0530

    fixes

--- routes/adminRoute/collectionRoute/collectionRoute.js
+++ routes/adminRoute/collectionRoute/collectionRoute.js
@@ -2,12 +2,17 @@ const express = require("express");
 const {
   handleCreateCollection,
   handleUpdateCollection,
-  handleDeleteCollection
+  handleDeleteCollection,
+  handleGetCollections,
+  handleGetCollectionById
 } = require("../../../controller/adminController/collections/collectionController");
 const upload = require("../../../cloudinaryService/upload");
 
 const router = express.Router();
 
+
+router.get("/collection", handleGetCollections);
+router.get("/collection/:id", handleGetCollectionById);
 router.post("/collection/add",upload.single('coverImageUrl'), handleCreateCollection);
 router.put("/collection/:id",upload.single('coverImageUrl'), handleUpdateCollection);
 router.delete("/collection/:id", handleDeleteCollection);
