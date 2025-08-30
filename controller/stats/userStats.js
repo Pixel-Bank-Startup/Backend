f7a7953df4f72fec15bd0fbe9f526195251cbc55
Satish Singh <i.sksingh113@gmail.com>
2025-08-30 13:08:35 +0530

    some fixes

--- controller/stats/userStats.js
+++ controller/stats/userStats.js
@@ -1,44 +1,32 @@
 const User = require("../../model/authModel/userModel");
-const Problem = require("../../model/problemModel/problem");
-const Statistics = require("../../model/userStatistics/stats");
+const Statistics  = require('../../model/userStatistics/stats')
 
 const updateFavoriteCategory = async (userId) => {
   try {
+    const user = await User.findById(userId)
+      .populate("solvedProblems.problemId", "category");
+    if (!user) return null;
 
-    const user = await User.findById(userId).populate("solvedProblems.problemId", "category");
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    if (!user.solvedProblems || user.solvedProblems.length === 0) {
-      return res.status(400).json({ message: "No solved problems for this user" });
-    }
+    if (!user.solvedProblems.length) return null;
 
     const categoryCount = {};
-    user.solvedProblems.forEach(entry => {
-      const category = entry.problemId?.category;
-      if (category) {
-        categoryCount[category] = (categoryCount[category] || 0) + 1;
-      }
+    user.solvedProblems.forEach(({ problemId }) => {
+      const category = problemId?.category;
+      if (category) categoryCount[category] = (categoryCount[category] || 0) + 1;
     });
 
-    let favoriteCategory = "";
-    let maxCount = 0;
-    for (const [category, count] of Object.entries(categoryCount)) {
-      if (count > maxCount) {
-        maxCount = count;
-        favoriteCategory = category;
-      }
-    }
+    const favoriteCategory = Object.keys(categoryCount).reduce((a, b) =>
+      categoryCount[a] > categoryCount[b] ? a : b
+    );
 
-    const stats = await Statistics.findOneAndUpdate(
+    return await Statistics.findOneAndUpdate(
       { user: userId },
       { favoriteCategory },
       { new: true, upsert: true }
     );
-
-    return res.status(200).json({ message: "Favorite category updated", stats });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "Server error", error });
+    throw error;
   }
 };
 
